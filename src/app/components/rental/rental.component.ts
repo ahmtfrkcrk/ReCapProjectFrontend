09f00caaeb1955922d0b6c95dccee9c3a084263d
FARUK <FARUK@FARUKPC>
2023-06-24 00:49:09 +0300

    24 haziran commit 1

---
+++ src/app/components/rental/rental.component.ts
@@ -0,0 +1,22 @@
+import { Component,OnInit } from '@angular/core';
+import { Rental } from 'src/app/models/rental';
+
+@Component({
+  selector: 'app-rental',
+  templateUrl: './rental.component.html',
+  styleUrls: ['./rental.component.css']
+})
+export class RentalComponent implements OnInit{
+
+  rentals:Rental[]=[];
+  dataLoaded=false;
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+
+  }
+
+}
